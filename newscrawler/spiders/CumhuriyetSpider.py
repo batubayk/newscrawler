d1bd1ef6c3c4a39658b843c577a93b68084659c4
batubayk <batuhanbaykara@yahoo.com>
2019-09-29 18:10:39 +0300

    updated spiders due to wrong handled exceptions

--- newscrawler/spiders/CumhuriyetSpider.py
+++ newscrawler/spiders/CumhuriyetSpider.py
@@ -33,13 +33,57 @@ class CumhuriyetSpider(CrawlSpider):
                 item['url'] = url
                 item['topic'] = topic
                 item['source'] = url[url.index('www.')+4:url.index('.com')]
-                item['author'] = response.xpath('//div[@class="publish-date"]/div[@class="left"]/span/a/text()').extract()[0]
-                item['date'] = response.xpath('//div[@class="publish-date"]/div[@class="right"]/span/text()').extract()[0]
-                item['title'] = ' '.join(response.xpath('//h1[@class="news-title"]/text()').extract())
-                item['abstract'] = ' '.join(response.xpath('//div[@class="news-short"]/text()').extract())
-                item['content'] = ' '.join(response.xpath('//div[@class="formatted news-content"]//p/text()').extract())
+                item['author'] = self.get_author(response)
+                item['date'] = self.get_date(response)
+                item['title'] = self.get_title(response)
+                item['abstract'] = self.get_abstract(response)
+                item['content'] = self.get_content(response)
                 # tags
             except Exception as e:
                 logging.error(e)
         return item
 
+    def get_author(self, response):
+        result = None
+        try:
+            result = response.xpath('//div[@class="publish-date"]/div[@class="left"]/span/a/text()').extract()[0]
+            return result
+        except Exception as e:
+            logging.error("Could not get author from URI: "+response.url)
+        return result
+
+    def get_content(self, response):
+        result = None
+        try:
+            result = ' '.join(response.xpath('//div[@class="formatted news-content"]//p/text()').extract())
+            return result
+        except Exception as e:
+            logging.error("Could not get content from URI: "+response.url)
+        return result
+
+    def get_abstract(self, response):
+        result = None
+        try:
+            result = ' '.join(response.xpath('//div[@class="news-short"]/text()').extract())
+            return result
+        except Exception as e:
+            logging.error("Could not get abstract from URI: "+response.url)
+        return result
+
+    def get_title(self, response):
+        result = None
+        try:
+            result = ' '.join(response.xpath('//h1[@class="news-title"]/text()').extract())
+            return result
+        except Exception as e:
+            logging.error("Could not get title from URI: "+response.url)
+        return result
+
+    def get_date(self, response):
+        result = None
+        try:
+            result = response.xpath('//div[@class="publish-date"]/div[@class="right"]/span/text()').extract()[0]
+            return result
+        except Exception as e:
+            logging.error("Could not get date from URI: "+response.url)
+        return result
--- newscrawler/spiders/NTVSpider.py
+++ newscrawler/spiders/NTVSpider.py
@@ -30,15 +30,77 @@ class NTVSpider(CrawlSpider):
             try:
                 url = str(response.url)
                 item['url'] = url
-                item['topic'] = response.xpath('//article/header/a/text()').extract()[1].replace("\r", "")
-                item['source'] = response.xpath('//article/header/p[@class="source"]/span/text()').extract()[0]
+                item['topic'] = self.get_topic(response)
+                item['source'] = self.get_source(response)
                 # item['author'] = response.xpath('//div[@class="publish-date"]/div[@class="left"]/span/a/text()').extract()[0]#
-                item['date'] = response.xpath('//article/header/time/span[@class="first-publish"]/span[@class="desktop-only"]/text()').extract()[0]
-                item['title'] = ' '.join(response.xpath('//article/header/div/h1/text()').extract())
-                item['abstract'] = ' '.join(response.xpath('//article/h2/text()').extract())
-                item['content'] = ' '.join(response.xpath('//article/div[@class="content article-body"]//p/text()').extract())
-                item['tags'] = response.xpath('//article/section[@class="tags"]/ul/li/a/text()').getall()
+                item['date'] = self.get_date(response)
+                item['title'] = self.get_title(response)
+                item['abstract'] = self.get_abstract(response)
+                item['content'] = self.get_content(response)
+                item['tags'] = self.get_tags(response)
             except Exception as e:
                 logging.error(e)
         return item
 
+    def get_tags(self, response):
+        result = None
+        try:
+            result = response.xpath('//article/section[@class="tags"]/ul/li/a/text()').getall()
+            return result
+        except Exception as e:
+            logging.error("Could not get tags from URI: "+response.url)
+        return result
+
+    def get_content(self, response):
+        result = None
+        try:
+            result = ' '.join(response.xpath('//article/div[@class="content article-body"]//p/text()').extract())
+            return result
+        except Exception as e:
+            logging.error("Could not get content from URI: "+response.url)
+        return result
+
+    def get_abstract(self, response):
+        result = None
+        try:
+            result = ' '.join(response.xpath('//article/h2/text()').extract())
+            return result
+        except Exception as e:
+            logging.error("Could not get abstract from URI: "+response.url)
+        return result
+
+    def get_title(self, response):
+        result = None
+        try:
+            result = ' '.join(response.xpath('//article/header/div/h1/text()').extract())
+            return result
+        except Exception as e:
+            logging.error("Could not get title from URI: "+response.url)
+        return result
+
+    def get_date(self, response):
+        result = None
+        try:
+            result = response.xpath('//article/header/time/span[@class="first-publish"]/span[@class="desktop-only"]/text()').extract()[0]
+            return result
+        except Exception as e:
+            logging.error("Could not get date from URI: "+response.url)
+        return result
+
+    def get_source(self, response):
+        result = None
+        try:
+            result = response.xpath('//article/header/p[@class="source"]/span/text()').extract()[0]
+            return result
+        except Exception as e:
+            logging.error("Could not get source from URI: "+response.url)
+        return result
+
+    def get_topic(self, response):
+        result = None
+        try:
+            result = response.xpath('//article/header/a/text()').extract()[1].replace("\r", "")
+            return result
+        except Exception as e:
+            logging.error("Could not get topic from URI: "+response.url)
+        return result
